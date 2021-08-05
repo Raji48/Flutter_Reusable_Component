dbccecf764d947e029230e0a1cfa9c794984e1b8
MAC-OBS-21 <mac-obs-21@MAC-OBS-21s-MacBook-Pro.local>
2021-08-05 13:40:15 +0530

    Circular Progress Loader inside Button

---
+++ Circular_Progress_Loader/CircularProgressButton.dart
@@ -0,0 +1,117 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:videoplugin_example/LinearProgressButton.dart';
+
+class CircularProgressButton extends StatefulWidget {
+  final String initialbtntext;
+  final String lodingbtntext;
+  final String successbtntext;
+  final Color btncolor;
+  final double btnheight;
+  final double btnwidth;
+  final Color txtcolor;
+  final double txtsize;
+  final int state;
+  const CircularProgressButton(this.initialbtntext,this.lodingbtntext,this.successbtntext,this.btncolor,
+      this.btnheight,this.btnwidth,this.txtcolor,this.txtsize,this.state) ;
+
+  @override
+  _CircularProgressButtonState createState() => _CircularProgressButtonState();
+}
+
+class _CircularProgressButtonState extends State<CircularProgressButton> with TickerProviderStateMixin{
+  // int _state = 0;
+  // Color txtclr=widget.txtcolor;
+  // double size=widget.txtsize;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Material(
+                  shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(22.0) ),
+                  clipBehavior: Clip.antiAlias,
+                  child: MaterialButton(
+
+                child: setUpButtonChild(),
+                onPressed: () {
+                  setState(() {
+                    if (widget.state == 0) {
+                      animateButton(widget.state);
+                    }
+                  });
+                },
+                elevation: 4.0,
+                minWidth: widget.btnwidth,
+                height: widget.btnheight,
+                color: widget.btncolor,
+              )),
+            )
+          ],
+        );
+
+  }
+
+  Widget setUpButtonChild() {
+
+    if (widget.state == 0) {
+      return new Text(
+        widget.initialbtntext,
+        style:  TextStyle(
+          color: widget.txtcolor,
+          fontSize: widget.txtsize,
+        ),
+      );
+    } else if (widget.state == 1) {
+      return
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircularProgressIndicator(
+            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          ),
+          Container(width: 20.0,),
+          Text(
+            "Loading....",
+            style:  TextStyle(
+              color: widget.txtcolor,
+              fontSize: widget.txtsize,
+            ),
+          )
+        ],
+      );
+    } else {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(Icons.check, color: Colors.white,size: 28.0,),
+          Container(width: 20.0,),
+          Text(
+            "Compeleted",
+            style:  TextStyle(
+              color: widget.txtcolor,
+              fontSize: widget.txtsize,
+            ),
+          )
+        ],
+      );
+    }
+  }
+
+  void animateButton(int state) {
+    setState(() {
+      state = 1;
+    });
+
+    Timer(Duration(milliseconds: 3300), () {
+      setState(() {
+        state = 2;
+      });
+    });
+  }
+}
