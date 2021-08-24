79bf6cb869fa5785cbdfc09a89a61422095acff4
Raji48 <rajeswari3699@gmail.com>
2021-08-24 16:44:07 +0530

    twitterlogin

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,98 @@
-//
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:twitter_login/twitter_login.dart';
+//import 'package:twitter_login_example/env.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: TextButton(
+                child: Text('login'),
+                style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                  backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent),
+                  minimumSize: MaterialStateProperty.all<Size>(Size(160, 48)),
+                ),
+                onPressed: () async {
+                  await login();
+                 // await signInWithTwitter();
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+  Future login() async {
+    final twitterLogin = TwitterLogin(
+      /// Consumer API keys
+      apiKey: 'eA71Q6zMCA923p3TCueR3yij5',
+
+      /// Consumer API Secret keys
+      apiSecretKey: 'WSdEyx3480Rc3RZGn4cgoBUMjW2FQASaLZlxFUr6MBE7moLzNX',
+
+      /// Registered Callback URLs in TwitterApp
+      /// Android is a deeplink
+      /// iOS is a URLScheme
+      redirectURI: 'https://twitterlogin-73c8f.firebaseapp.com/__/auth/handler',
+    );
+
+    /// Forces the user to enter their credentials
+    /// to ensure the correct users account is authorized.
+    /// If you want to implement Twitter account switching, set [force_login] to true
+    /// login(forceLogin: true);
+    final authResult = await twitterLogin.login();
+    switch (authResult.status) {
+      case TwitterLoginStatus.loggedIn:
+      // success
+        print('====== Login success ======');
+        break;
+      case TwitterLoginStatus.cancelledByUser:
+      // cancel
+        print('====== Login cancel ======');
+        break;
+      case TwitterLoginStatus.error:
+      case null:
+      // error
+        print('====== Login error ======');
+        break;
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
 // import 'dart:async';
 // import 'package:flutter/material.dart';
 // import 'package:flutter/services.dart';
@@ -199,141 +293,3 @@
 //   }
 //
 //   }
-//
-//
-
-
-
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-//import 'package:flutter_facebook_login/flutter_facebook_login.dart';
-import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
-import 'package:flutter_twitter_login/flutter_twitter_login.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: MyMainPage(),
-  ));
-}
-
-class MyMainPage extends StatefulWidget {
-  @override
-  _MyMainPageState createState() => _MyMainPageState();
-}
-
-class _MyMainPageState extends State<MyMainPage> {
-  FirebaseAuth _auth = FirebaseAuth.instance;
-  bool isLogged = false;
-
-  FirebaseUser myUser;
-
-  // Future<FirebaseUser> _loginWithFacebook() async {
-  //   var facebookLogin = new FacebookLogin();
-  //   var result = await facebookLogin.logInWithReadPermissions(['email']);
-  //
-  //   debugPrint(result.status.toString());
-  //
-  //   if (result.status == FacebookLoginStatus.loggedIn) {
-  //     FirebaseUser user =
-  //     await _auth.signInWithFacebook(accessToken: result.accessToken.token);
-  //     return user;
-  //   }
-  //   return null;
-  // }
-
-  Future<FirebaseUser> _loginWithTwitter() async {
-    var twitterLogin = new TwitterLogin(
-      consumerKey: 'eA71Q6zMCA923p3TCueR3yij5',
-      consumerSecret: 'WSdEyx3480Rc3RZGn4cgoBUMjW2FQASaLZlxFUr6MBE7moLzNX',
-    );
-
-    final TwitterLoginResult result = await twitterLogin.authorize();
-
-    switch (result.status) {
-      case TwitterLoginStatus.loggedIn:
-        var session = result.session;
-        FirebaseUser user = await _auth.signInWithTwitter(
-            authToken: session.token, authTokenSecret: session.secret);
-        return user;
-        break;
-      case TwitterLoginStatus.cancelledByUser:
-        debugPrint(result.status.toString());
-        return null;
-        break;
-      case TwitterLoginStatus.error:
-        debugPrint(result.errorMessage.toString());
-        return null;
-        break;
-    }
-    return null;
-  }
-
-  void _logOut() async {
-    await _auth.signOut().then((response) {
-      isLogged = false;
-      setState(() {});
-    });
-  }
-
-  // void _logIn() {
-  //   _loginWithFacebook().then((response) {
-  //     if (response != null) {
-  //       myUser = response;
-  //       isLogged = true;
-  //       setState(() {});
-  //     }
-  //   });
-  // }
-
-  void _logInTwitter() {
-    _loginWithTwitter().then((response) {
-      if (response != null) {
-        myUser = response;
-        isLogged = true;
-        setState(() {});
-      }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(isLogged ? "Profile Page" : "Facebook Loing Example"),
-        actions: <Widget>[
-          isLogged
-              ? IconButton(
-            icon: Icon(Icons.power_settings_new),
-            onPressed: _logOut,
-          )
-              : IconButton(
-            icon: Icon(Icons.remove_red_eye),
-            onPressed: () {},
-          ),
-        ],
-      ),
-      body: Center(
-        child: isLogged
-            ? Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('Name: ' + myUser.displayName),
-            Image.network(myUser.photoUrl),
-          ],
-        )
-            : Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            // FacebookSignInButton(
-            //   onPressed: _logIn,
-            // ),
-            SizedBox(height: 30.0),
-            TwitterSignInButton(
-              onPressed: _logInTwitter,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
