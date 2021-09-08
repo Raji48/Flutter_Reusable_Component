9012afa6d54665d751b28eacd0608da5604c8767
Abisheik-IAN <mac-obs-21@MAC-OBS-21s-MacBook-Pro.local>
2021-09-08 10:00:18 +0530

    login

---
+++ lib/SocialLogin/Components/Button.dart
@@ -0,0 +1,54 @@
+
+import 'package:expensesapproval/res/colors.dart';
+import 'package:flutter/material.dart';
+
+class CustomButtom extends StatefulWidget {
+  final GestureTapCallback onPressed;
+//  final bool showIcon;
+  final String btnName;
+  final double width;
+  final double height;
+  final Color btnColor;
+  final Color txtColor;
+  final IconData  icon;
+
+  CustomButtom( this.btnName, this.width, this.height, this.onPressed,this.btnColor,this.txtColor,this.icon);
+  _CustomeButtom createState() => _CustomeButtom();
+}
+
+class _CustomeButtom extends State<CustomButtom> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+        children: <Widget> [
+          Material(
+              child: InkWell(
+                  onTap: (){
+                    widget.onPressed();
+                  },
+                  child: Container(
+                    width: widget.width,
+                    height: widget.height,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: widget.btnColor,
+                    ),
+                    child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget> [
+                          Text(widget.btnName,
+                            style: TextStyle(fontFamily: 'Work Sans', color: widget.txtColor, fontSize: 16,fontWeight: FontWeight.bold),
+                          ),
+                          (widget.icon!=null)? Icon(widget.icon,color: white,size: 16.0,):Container()
+//                          widget.showIcon ? (SizedBox(width: 40)) : Container(),
+//                          widget.showIcon ? (Icon(Icons.arrow_forward, size: 30, color: white,)) : Container()
+                        ]
+                    ),
+                  )
+              )
+          )
+        ]
+    );
+  }
+}
---
+++ lib/SocialLogin/Utilis/dynamicheigthwidth.dart
@@ -0,0 +1,90 @@
+
+
+import 'dart:io';
+
+
+import 'package:flutter/material.dart';
+
+getDevicewidth(BuildContext context,double percentage) {
+//  print(MediaQuery.of(context).size.width);
+  var width=MediaQuery.of(context).size.width;
+  double wid=width*percentage;
+
+  return wid;
+}
+
+getDeviceheight(BuildContext context,double percentage) {
+//  print(MediaQuery.of(context).size.height);
+  var height= MediaQuery.of(context).size.height;
+  double heg=height*percentage;
+
+  return heg;
+
+
+}
+
+getheightwithoutsafeArea(BuildContext context,int percentage) {
+  // height without SafeArea
+  var padding = MediaQuery.of(context).padding;
+  var height = MediaQuery.of(context).size.height;
+  var sheight= height - padding.top - padding.bottom;
+  double heg=sheight*percentage;
+
+  return heg;
+}
+
+getheightwithoutstatusBar(BuildContext context,int percentage) {
+  // height without status bar
+  var padding = MediaQuery.of(context).padding;
+  var height = MediaQuery.of(context).size.height;
+  var sheight= height - padding.top;
+  double heg=sheight*percentage;
+
+  return heg;
+}
+
+getheightwithoutStatusToolBar(BuildContext context,int percentage) {
+  // height without status and toolbar
+  var padding = MediaQuery.of(context).padding;
+  var height = MediaQuery.of(context).size.height;
+  var sheight= height - padding.top - kToolbarHeight;
+  double heg=sheight*percentage;
+
+  return heg;
+}
+
+isEmpty(text) {
+  return (
+      text.trim().length == 0 ||
+          text == null ||
+          text == "null" ||
+          text == ""
+  );
+}
+
+isvalidateEmail(mail) {
+  Pattern  pattern = r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+";
+  RegExp regExp = new RegExp(pattern);
+  return regExp.hasMatch(mail);
+}
+
+isvalidateName(text) {
+  Pattern  pattern = r"^[a-zA-Z](\/?[0-9a-z])*";
+  RegExp regExp = new RegExp(pattern);
+  return regExp.hasMatch(text);
+}
+
+isValidPassword(text) {
+  Pattern pattern = r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+  RegExp regExp = new RegExp(pattern);
+  return regExp.hasMatch(text);
+}
+isValidNumber(text){
+  Pattern pattern=r'^([1-9][0-9]*)';
+  RegExp regExp =RegExp(pattern);
+  return regExp.hasMatch(text);
+}
+
+isAndroid() {
+  return (Platform.isAndroid);
+}
